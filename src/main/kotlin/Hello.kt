c5d1e04ef5ee2ba81669b8ad5d5bd2c596a38e4d
Daniel Stutzman <dtstutz@gmail.com>
2017-07-27 15:00:11 -0600

    Demonstrate Postgresql connection

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,7 +1,21 @@
 import com.sun.net.httpserver.HttpServer
 import java.net.InetSocketAddress
+import java.sql.DriverManager
+
 
 fun main(args: Array<String>) {
+  val jdbcUrl = "jdbc:postgresql://localhost:5432/todo-go"
+  Class.forName("org.postgresql.Driver")
+  val conn = DriverManager.getConnection(jdbcUrl, "dan", "")
+  val stmt = conn.createStatement()
+  val rset = stmt.executeQuery("SELECT 1")
+  while (rset.next()) {
+    println(rset.getString(1))
+  }
+  rset.close()
+  stmt.close()
+  conn.close()
+
   println("Starting server on 8000...")
   val server = HttpServer.create(InetSocketAddress(8000), 0);
   server.createContext("/", MyHandler())
