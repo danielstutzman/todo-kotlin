1eae8f0711b5a6332d30c7ccdf64b8c72ecfccea
Daniel Stutzman <dtstutz@gmail.com>
2017-07-27 15:44:46 -0600

    Update whitespace

--- src/main/kotlin/MyHandler.kt
+++ src/main/kotlin/MyHandler.kt
@@ -1,17 +1,16 @@
+import com.fizzed.rocker.runtime.OutputStreamOutput
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpHandler
-import com.fizzed.rocker.runtime.ArrayOfByteArraysOutput
-import com.fizzed.rocker.runtime.OutputStreamOutput
 
 class MyHandler : HttpHandler {
   override fun handle(exchange: HttpExchange) {
     exchange.sendResponseHeaders(200, 0)
 
     views.index
-      .template("World")
-      .render({ contentType, charsetName ->
-        OutputStreamOutput(contentType, exchange.getResponseBody(), charsetName)
-      })
+        .template("World")
+        .render({ contentType, charsetName ->
+          OutputStreamOutput(contentType, exchange.getResponseBody(), charsetName)
+        })
 
     exchange.getResponseBody().close()
   }
