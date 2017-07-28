2835d107051af11627eee79d3bb09c3948a94859
Daniel Stutzman <dtstutz@gmail.com>
2017-07-27 22:09:28 -0600

    Demo of SQL inserting a row

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -2,18 +2,43 @@ import com.sun.net.httpserver.HttpServer
 import java.io.File
 import java.net.InetSocketAddress
 import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.Statement
+
 
 fun testDatabase(creds: PostgresCredentials) {
   val jdbcUrl = "jdbc:postgresql://${creds.hostname}:${creds.port}/${creds.databaseName}"
-  val conn = DriverManager.getConnection(jdbcUrl, creds.username, creds.password)
-  val stmt = conn.createStatement()
-  val rset = stmt.executeQuery("SELECT 1")
-  while (rset.next()) {
-    println(rset.getString(1))
+  DriverManager.getConnection(jdbcUrl, creds.username, creds.password).let { conn ->
+    /*
+    conn.createStatement().let { stmt ->
+      stmt.executeQuery("SELECT 1").let { rset ->
+        while (rset.next()) {
+          println(rset.getString(1))
+        }
+        rset.close()
+      }
+      stmt.close()
+    }
+    */
+    val sql = "INSERT INTO users (a) VALUES (?)"
+    conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS).let { stmt ->
+      stmt.setInt(1, 5)
+      stmt.executeUpdate().let { numRows ->
+        if (numRows != 1) {
+          throw RuntimeException("executeUpdate returned ${numRows}")
+        }
+      }
+      stmt.getGeneratedKeys().let { rset: ResultSet ->
+        if (!rset.next()) {
+          throw RuntimeException("Can't find generated ID of insert")
+        }
+        val key = rset.getInt(1)
+        println("PK is ${key}")
+        rset.close()
+      }
+    }
+    conn.close()
   }
-  rset.close()
-  stmt.close()
-  conn.close()
 }
 
 fun main(args: Array<String>) {
