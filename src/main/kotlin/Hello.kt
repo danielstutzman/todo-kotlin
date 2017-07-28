b85322590a4e45b37fc4587d2717d230211a0462
Daniel Stutzman <dtstutz@gmail.com>
2017-07-28 00:33:10 -0600

    Jooq example for delete, insert, select

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,9 +1,11 @@
 import com.sun.net.httpserver.HttpServer
+import org.jooq.SQLDialect
+import org.jooq.generated.Tables.USERS
+import org.jooq.impl.DSL
 import java.io.File
 import java.net.InetSocketAddress
 import java.sql.DriverManager
-import java.sql.ResultSet
-import java.sql.Statement
+import java.sql.Timestamp
 
 
 fun testDatabase(creds: PostgresCredentials) {
@@ -20,6 +22,8 @@ fun testDatabase(creds: PostgresCredentials) {
       stmt.close()
     }
     */
+
+    /*
     val sql = "INSERT INTO users (a) VALUES (?)"
     conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS).let { stmt ->
       stmt.setInt(1, 5)
@@ -37,6 +41,38 @@ fun testDatabase(creds: PostgresCredentials) {
         rset.close()
       }
     }
+    */
+
+    System.setProperty("org.jooq.no-logo", "true")
+
+    DSL.using(conn, SQLDialect.POSTGRES_9_5).let { create ->
+      create.delete(USERS)
+          .where(USERS.EMAIL.eq("a1"))
+          .execute()
+
+      val record = create.insertInto(USERS,
+          USERS.EMAIL,
+          USERS.ENCRYPTED_PASSWORD,
+          USERS.CREATED_AT,
+          USERS.UPDATED_AT)
+          .values("a1",
+              "b1",
+              Timestamp(System.currentTimeMillis().toLong()),
+              Timestamp(System.currentTimeMillis().toLong()))
+          .returning(USERS.ID)
+          .fetchOne()
+      val newId = record.getValue(USERS.ID)
+      println("Created record with ID ${newId}")
+
+      create.select().from(USERS).fetch().let { rset ->
+        for (row in rset) {
+          val id = row.getValue(USERS.ID)
+          val email = row.getValue(USERS.EMAIL)
+          println("ID: $id EMAIL: $email")
+        }
+      }
+    }
+
     conn.close()
   }
 }
