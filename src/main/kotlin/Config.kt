2f4341d649cd0acf01da3b49401987ca12451ff3
Daniel Stutzman <dtstutz@gmail.com>
2017-07-29 01:20:21 -0600

    Make sure nulls can't get in Config

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -1,5 +1,6 @@
 import com.google.gson.Gson
 
+
 data class PostgresCredentials(
     val hostname: String,
     val port: Int,
@@ -9,42 +10,50 @@ data class PostgresCredentials(
     val databaseName: String
 )
 
+data class PostgresCredentialsNulls(
+    val hostname: String?,
+    val port: Int?,
+    val sslMode: String?,
+    val username: String?,
+    val password: String?,
+    val databaseName: String?
+) {
+  fun toPostgresCredentials() = PostgresCredentials(
+      hostname ?: throw RuntimeException("Missing hostname"),
+      port ?: throw RuntimeException("Missing port"),
+      sslMode ?: throw RuntimeException("Missing sslMode"),
+      username ?: throw RuntimeException("Missing username"),
+      password ?: throw RuntimeException("Missing password"),
+      databaseName ?: throw RuntimeException("Missing databaseName"))
+}
+
+
 data class Config(
     val port: Int,
     val postgresCredentials: PostgresCredentials
 )
 
+data class ConfigNulls(
+    val port: Integer,
+    val postgresCredentials: PostgresCredentialsNulls?
+) {
+  fun toConfig() = Config(
+      if (port != null)
+        port.toInt()
+      else
+        throw RuntimeException("Missing port"),
+      if (postgresCredentials != null)
+        postgresCredentials.toPostgresCredentials()
+      else
+        throw RuntimeException("Missing postgresCredentials")
+
+  )
+}
+
 /** @Throws com.google.gson.JsonSyntaxException */
-
-// suppress warning from comparing String to null
-@Suppress("SENSELESS_COMPARISON")
-
 fun parseConfigJson(json: String): Config {
-  val config: Config = Gson().fromJson(json, Config::class.java)
-  if (config.port == 0) {
-    throw RuntimeException("Missing port")
-  }
-  if (config.postgresCredentials == null) {
-    throw RuntimeException("Missing postgresCredentials")
-  }
-  if (config.postgresCredentials.hostname == null) {
-    throw RuntimeException("Missing postgresCredentials.hostname")
-  }
-  if (config.postgresCredentials.port == null) {
-    throw RuntimeException("Missing postgresCredentials.port")
-  }
-  if (config.postgresCredentials.sslMode == null) {
-    throw RuntimeException("Missing postgresCredentials.sslMode")
-  }
-  if (config.postgresCredentials.databaseName == null) {
-    throw RuntimeException("Missing postgresCredentials.databaseName")
-  }
-  if (config.postgresCredentials.username == null) {
-    throw RuntimeException("Missing postgresCredentials.username")
-  }
-  if (config.postgresCredentials.password == null) {
-    throw RuntimeException("Missing postgresCredentials.password")
-  }
-  return config
+  val configNulls: ConfigNulls =
+      Gson().fromJson(json, ConfigNulls::class.java)
+  return configNulls.toConfig()
 }
 
